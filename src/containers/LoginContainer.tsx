33aad3e03443a7e170c42d0803e0d37abb99ddec
695954085 <695954085@qq.com>
2019-01-20 10:02:16 +0800

    fix: leakage of submit

---
+++ src/containers/LoginContainer.tsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { fetchLogin as fetchLoginMethod } from '../actions/login.action';
+import Login from '../components/Login';
+import { getErrorMessage, getFetching } from '../reducers/login.reducer';
+
+const LoginContainer = ({
+  fetchLogin,
+  isFetching,
+  error,
+}: ConnectLoginContainerPropTypes) => (
+  <Login onSubmit={fetchLogin} isFetching={isFetching} error={error} />
+);
+
+LoginContainer.propTypes = {
+  fetchLogin: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state: ReducerType) => ({
+  error: getErrorMessage(state),
+  isFetching: getFetching(state),
+});
+
+export default connect<
+  LoginContainerStateTypes,
+  LoginContainerPropTypes,
+  {},
+  ReducerType
+>(
+  mapStateToProps,
+  {
+    fetchLogin: fetchLoginMethod,
+  },
+)(LoginContainer);
